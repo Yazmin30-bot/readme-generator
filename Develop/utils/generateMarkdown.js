ef69b0eb38cf67f47609c40562e0b18db866d1e3
Yazmin Tinoco <ytip30@gmail.com>
2021-05-14 15:43:55 -0500

    Create a function that returns the license section of README file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -32,7 +32,16 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license != "None") {
+    
+    return [`*[License](#license)`,`## License
+  
+  This project is licensed under the [${license}](${renderLicenseLink(license)}) license.`];
+  } else {
+    return [``,``];
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
